7ffdb3b0c3a208f968fa65fc1a5d741a42f3e59c
biebishan <360255931@qq.com>
2021-04-19 16:43:05 +0800

    去掉了无用代码

--- src/server.js
+++ src/server.js
@@ -32,25 +32,4 @@ app.get('/getData', (req, res) => {
   // })
 })
 app.post('/login', (req, res) => res.send('Hello World!'))
-app.get('/wx', (req, res, next) => {
-  const token = 'wxToken';
-  let signature =  req.query.signature
-  let timestamp  =  req.query.timestamp 
-  let nonce  =  req.query.nonce 
-  let echostr  =  req.query.echostr 
-  let arr = [token, timestamp, nonce]
-  arr.sort()
-  let tempStr = arr.join('');
-  let shaObj = new jsSHA('SHA-1', 'TEXT');
-  shaObj.update(tempStr);
-  let scyptoString=shaObj.getHash('HEX');
-   //4.开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
-   if(signature === scyptoString){
-    console.log('验证成功')
-      res.send(echostr);
-  }else{
-    console.log('验证失败')
-      res.send('验证失败');
-  }
-})
 app.listen(80, () => console.log('Example app listening on port 80'))
